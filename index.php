1fc6d54c13f133f94cd4e2a9cf392597c4b37fb2
pledgeco <vegard@vevit.no>
2014-09-28 22:58:12 +0200

    Index and CSS
    Coded and designet Index. This will be the main design that will be
    reflected throughout the entire page.

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Bruktbok.no</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+    <div id="header"> <!--her er header definert-->
+    	<div class="wrapper"> <!--er en klasse som definerer en bredde på midtseksjonen vår-->
+            <a id="logo" href="/">Bruktbok.no</a>
+		<ul id="nav"><!--meny-->
+                <li><a href="index.php">Hjem</a></li>
+                <li><a href="register.php">Registrer deg</a></li>
+                <li><a href="contact.php">Kontakt oss</a></li>
+		<li><a href="login.php">Logg inn</a></li>
+		</ul>
+           </div> </div>
+       
+       
+    <div id="eyeCatcher">
+    	
+            <div class="column"> <!--definerer klassen kolonnen-->
+		<a href="/register.php" style="color:white;"><img src="images/form.png" /><br />
+		<h3>1. Registrer deg</h3></a>
+		<span style="color: #c7c4c4">Registreringen tar
+			under et minutt. Vi trenger kun litt
+			informasjon om deg.</span>			
+		</div>
+		
+            <div class="column"> <!--definerer kolonnen-->
+		<img src="images/bok.png" /><br />
+		<h3>2. Legg ut bøker</h3>
+		<span style="color: #c7c4c4">Du kan selge alle bøker som tilhører UIA i Grimstad og Kristiansand. 
+		</span> 
+            </div>
+	    
+            <div class="column" style="margin-right: 0;"> <!--definerer kolonnen-->
+		<img src="images/hammer.png" /><br />
+		<h3>3. Solgt!</h3>
+		<span style="color: #c7c4c4">Når bøkene dine er lagt ut for salg, så er det bare å vente på at noen vil kjøpe dem. 
+		</span>
+            </div>
+        </div>
+    </div>
+    
+    <div id="midLine">
+	<div class="wrapper">
+			<a href="register.php" id="midButton"></a>
+			<p class="text">Selg eller kjøp studiebøker via Bruktbok.no med et par enkle tastetrykk.</p>            
+		</div>
+	</div>
+	
+	<div class="wrapper"><!--definerer bredden på nytt-->
+		<p class="mainHeader">Hva er Bruktbok?</p>
+			<div id="recycle"></div>
+				<div class="wrapper2"> <!--definerer bredde for tekst inne i første wrapper-->
+					<p class="mainText">Vi vet alle at millioner av trær blir hogget ned hvert år for å lage papir for bøker,
+					magasiner og mye mer. Ettersom etterspørselen etter bøker er stor, så er antallet trær som blir hugget ned tilsvarende.
+					Hvordan kan vi stoppe denne onde sirkelen? Det er umulig å slutte å lese og skrive. Dette er en av grunnene til at vi i 
+					Bruktbok.net ble motiverte til å lage en applikasjon som kan gjøre det enkelt å bruke bøker om igjen. På våre sider skal du kunne legge ut dine brukte bøker,
+					og ikke minst finne bøker du vil kjøpe. Du sparer miljøet, og du sparer også penger.</p>
+				</div>
+			</div>
+		</div>
+      
+	<div id="footer"><i>Alle rettigheter reservert © 2012 |
+	<a href="contact.php">Kontakt oss</a> - <a href="terms.php">Terms of Use </a> - <a href="privacy.php">Privacy</a></i>
+    </div>
+</body>
+</html>
